bb063e9511bf43dad4465a87b900f19c98ddd645
Ольга Мельникова <olgamelnikova@Olgas-MacBook-Pro.local>
2022-03-25 17:16:05 +0300

    start project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+var config = Configuration.Default.WithDefaultLoader();
+using var context = BrowsingContext.New(config);
+
+var url = "https://www.toy.ru/catalog/boy_transport/";
+
+using var doc = await context.OpenAsync(url);
+var div = doc.QuerySelector("div.select-city-link");
+Console.WriteLine(div.TextContent.Split(":").Last().Trim());
